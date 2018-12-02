f9e01f6b1bf867ec9c084d05f507f8c2f3ca4d4a
veronique monaco <veronique.jollivel@wanadoo.fr>
2018-12-02 21:33:50 +0100

    correction panier qte 0 et css

--- src/AppBundle/Controller/PanierController.php
+++ src/AppBundle/Controller/PanierController.php
@@ -22,8 +22,16 @@ class PanierController extends Controller
         if($session->has('panier'))
             $panier = $session->get('panier');
 
+        $panierCorrige = [];
+
+        foreach ($panier as $index=>$addproduct) {
+            if ($addproduct->getQuantity() != 0) {
+                $panierCorrige[$index]=$addproduct;
+            }
+        }
+
         return $this->render('front/panier.html.twig', array(
-            'panier' => $panier,
+            'panier' => $panierCorrige,
             'user' => $user
         ));
     }
